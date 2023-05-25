f612684148dc61634da42d2c0c7f32bb4e6a9761
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-05-25 14:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,15 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	var codearr = encodedStr.split("");
+	decodedArr = codearr.map(function(letter){
+		if(lookup.hasOwnProperty(letter)){
+			letter =lookup[letter];
+		}
+		return letter;
+	});
 
-  return ;//return decodedArr
+  return decodedArr.join(" ") ;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
